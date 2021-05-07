f95b1aad6d012aecd0f392ac586e3926a5bbf78a
Eduard Capanu <eduard.capanu@talentium.it>
2021-05-07 18:10:18 +0200

    feat: some styling on the top box of header body

--- src/google-keep/body/components/Header.tsx
+++ src/google-keep/body/components/Header.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { brush, check_box, image, pin } from "../../../core/google-icons";
+import React, { FocusEventHandler } from 'react';
+import {
+  add_alert,
+  archive,
+  brush,
+  check_box,
+  image,
+  more_vert,
+  palette,
+  persona_add_alt,
+  pin, redo
+} from "../../../core/google-icons";
 import { useOnClickOutside } from "../../../core/hooks/useOnClickOutside";
+import { setNavbarIsBodyAtTop } from "../../../store/reducers/layout.store";
+
+const MAX_LENGTH_CHAR_CONTENT = 999;
 
 export const Header: React.FC = () => {
   const [isContentEditableFocus, setIsContentEditableFocus] = React.useState(false);
+  const bodyHeaderInputRef = React.useRef<HTMLDivElement>(null);
+  const bodyHeaderInputTitleRef = React.useRef<HTMLDivElement>(null);
 
   const setIsContentEditableFocusHandler = React.useCallback((value) => {
     setIsContentEditableFocus(value);
@@ -12,15 +27,74 @@ export const Header: React.FC = () => {
   const headerBoxRef = React.useRef<any | null>(null);
   useOnClickOutside(headerBoxRef, () => setIsContentEditableFocusHandler(false));
 
+  const [isBodyAtTop, setIsBodyAtTop] = React.useState(true);
+  const [isBodyAtBottom, setIsBodyAtBottom] = React.useState(true);
+
+  const setIsBodyAtTopHandler = React.useCallback((value) => {
+    setIsBodyAtTop(value)
+  }, []);
+  const setIsBodyAtBottomHandler = React.useCallback((value) => {
+    setIsBodyAtBottom(value)
+  }, []);
+
+  const onScrollBodyHandler = (e: any) => {
+    const st = e.currentTarget.scrollTop;
+    const sh = e.currentTarget.scrollHeight;
+    const ht = e.currentTarget.offsetHeight;
+    if (st === 0) {
+      setIsBodyAtTopHandler(true)
+    } else if ((ht === 0) || (Math.ceil(st) === (sh - ht)) || (Math.round(st) === (sh - ht)) || (Math.floor(st) === (sh - ht))) {
+      setIsBodyAtBottomHandler(true)
+    } else {
+      if (isBodyAtTop)
+        setIsBodyAtTopHandler(false)
+      if (isBodyAtBottom)
+        setIsBodyAtBottomHandler(false)
+    }
+  }
+
+  const bodyHeaderInputContainerRef = React.useRef<HTMLDivElement>(null);
+  const onKeyDownHandler = React.useCallback((event: React.KeyboardEvent<HTMLDivElement>) => {
+    const target = (event.target) as HTMLElement;
+
+    if (event.code === "Backspace" && bodyHeaderInputContainerRef?.current?.scrollTop === 0) {
+      setIsBodyAtBottom(true)
+    }
+
+    if (event.code === "Backspace" && target.innerText.trim() === "" && bodyHeaderInputContainerRef.current) {
+
+    }
+  }, []);
+
+  const onKeyDownBodyHeaderInputTitleHandler = React.useCallback((event: React.KeyboardEvent<HTMLDivElement>) => {
+    const target = (event.target) as HTMLElement;
+
+    if (MAX_LENGTH_CHAR_CONTENT && (target.innerText.trim().length > MAX_LENGTH_CHAR_CONTENT) && event.code !== "Backspace")
+      event.preventDefault();
+
+    if ((event.keyCode === 13 && !event.shiftKey) || (event.code === "Enter" && !event.shiftKey)) {
+     bodyHeaderInputRef?.current?.focus({preventScroll: true})
+    }
+
+    if (event.code === "Backspace") {
+      console.log("target.innerText.trim(): ", target.innerText.trim())
+    }
+  }, []);
+
+  const onFocusBodyHeaderInputHandler = (e: any) => {
+    setIsContentEditableFocusHandler(true)
+  }
+
   return <div className="rx-body-header-container">
-    <div className={`rx-body-header-box ${isContentEditableFocus ? 'open' : ''}`} ref={headerBoxRef}>
+    <div className={`rx-body-header-box ${isContentEditableFocus ? 'open' : ''} mini-scrollbar`} ref={headerBoxRef}>
 
       {
         isContentEditableFocus &&
-        <div className="rx-body-header-box-top-container">
-          <div className="rx-body-header-action-pinned"><div className="icon-container small">{pin}</div></div>
+        <div className={`rx-body-header-box-top-container ${!isBodyAtTop ? 'shadow' : ''}`}>
+          <div className="rx-body-header-action-pinned">
+            <div className="icon-container small">{pin}</div>
+          </div>
           <div className="rx-body-header-icon-separator"/>
-
           <div className="rx-body-header-input-container">
             <div className="rx-body-header-input title">
               <div
@@ -29,22 +103,27 @@ export const Header: React.FC = () => {
                 placeholder="Titolo"
                 onFocus={() => setIsContentEditableFocusHandler(true)}
                 // onBlur={() => setIsContentEditableFocusHandler(false)}
+                ref={bodyHeaderInputTitleRef}
+                onKeyDown={onKeyDownBodyHeaderInputTitleHandler}
               />
             </div>
           </div>
-
-
-
         </div>
       }
 
-      <div className="rx-body-header-input">
+      <div className={`rx-body-header-input ${!isContentEditableFocus ? 'full-width' : ''}`}
+           onScroll={onScrollBodyHandler}
+           ref={bodyHeaderInputContainerRef}
+      >
         <div
+          id="editable"
           contentEditable="true"
           className="rx-text-box"
           placeholder="Scrivi una nota..."
-          onFocus={() => setIsContentEditableFocusHandler(true)}
-          //onBlur={() => setIsContentEditableFocusHandler(false)}
+          onFocus={onFocusBodyHeaderInputHandler}
+          ref={bodyHeaderInputRef}
+          onKeyDown={onKeyDownHandler}
+
         />
       </div>
 
@@ -59,12 +138,19 @@ export const Header: React.FC = () => {
 
       {
         isContentEditableFocus &&
-        <React.Fragment>
-          <div>
-            actions
+        <div className={`rx-body-header-footer ${!isBodyAtBottom ? 'shadow' : ''}`}>
+          <div className="rx-body-header-footer-actions">
+            <div className="icon-container small">{add_alert}</div>
+            <div className="icon-container small">{persona_add_alt}</div>
+            <div className="icon-container small">{palette}</div>
+            <div className="icon-container small">{image}</div>
+            <div className="icon-container small">{archive}</div>
+            <div className="icon-container small">{more_vert}</div>
+            <div className="icon-container small">{redo}</div>
+            <div className="icon-container small scale-to-right">{redo}</div>
           </div>
-          <div>Chiudi</div>
-        </React.Fragment>
+          <div className="rx-body-header-footer-button"><div className="rx-button-transparent">Chiudi</div></div>
+        </div>
       }
     </div>
   </div>
