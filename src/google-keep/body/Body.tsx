99ffe743de875961d8bf20e8ba87163cfb1ee2d2
razxssd <capanueduard98@gmail.com>
2021-05-09 22:15:07 +0200

    feat: adding masonry lib

--- src/google-keep/body/Body.tsx
+++ src/google-keep/body/Body.tsx
@@ -1,16 +1,80 @@
 import React from 'react';
-import { Header } from "./components/Header";
+import {Header} from "./components/Header";
+import Masonry from "masonry-layout";
 
 export const Body: React.FC = () => {
+
+  React.useEffect(() => {
+    var elem = document.querySelector('.rx-masonry-grid');
+    if (elem)
+      var msnry = new Masonry(elem, {
+        // options
+        itemSelector: '.rx-masonry-grid-item',
+        columnWidth: 140
+      });
+  }, []);
+
+
   return <div className='rx-body-container'>
     <Header/>
 
     <div className="rx-body-content-container">
-      <div>Card</div>
-      <div>Card</div>
-      <div>Card</div>
-      <div>Card</div>
-      <div>Card</div>
+      <div className="rx-masonry-grid">
+
+
+        <div className="rx-masonry-grid-item">
+          <div className="rx-card">
+            Card
+            lorem ipsum lorem dolorem lorem ipsum lorem dolorem
+            lorem ipsum lorem dolorem
+            lorem ipsum lorem doloremlorem ipsum lorem dolorem
+            lorem ipsum lorem dolorem
+            lorem ipsum lorem dolorem
+            lorem ipsum lorem dolorem lorem ipsum lorem dolorem
+            lorem ipsum lorem dolorem
+            lorem ipsum lorem doloremlorem ipsum lorem dolorem
+            lorem ipsum lorem dolorem
+            lorem ipsum lorem dolorem
+            lorem ipsum lorem dolorem lorem ipsum lorem dolorem
+            lorem ipsum lorem dolorem
+            lorem ipsum lorem doloremlorem ipsum lorem dolorem
+            lorem ipsum lorem dolorem
+            lorem ipsum lorem dolorem
+          </div>
+        </div>
+        <div className="rx-masonry-grid-item">
+          <div className="rx-card">Card lorem ipsum
+            adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+          </div>
+        </div>
+        <div className="rx-masonry-grid-item">
+          <div className="rx-card">adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+          </div>
+        </div>
+        <div className="rx-masonry-grid-item">
+          <div className="rx-card">adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+          </div>
+        </div>
+        <div className="rx-masonry-grid-item">
+          <div className="rx-card">
+            adsdasdad askdas daksd ajsd ajsdk asdj
+            adsdasdad askdas daksd ajsd ajsdk asdj
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 };
