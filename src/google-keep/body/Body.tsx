4dcdc846e2025fdf6f00458491f888edebccd6b4
razxssd <capanueduard98@gmail.com>
2021-05-09 23:37:19 +0200

    temp: niente

--- src/google-keep/body/Body.tsx
+++ src/google-keep/body/Body.tsx
@@ -10,7 +10,15 @@ export const Body: React.FC = () => {
       var msnry = new Masonry(elem, {
         // options
         itemSelector: '.rx-masonry-grid-item',
-        columnWidth: 140
+        columnWidth: 200
+      });
+
+    var elem_2 = document.querySelector('.grid');
+    if (elem_2)
+      var msnry_2 = new Masonry(elem_2, {
+        // options
+        itemSelector: '.grid-item',
+        columnWidth: 200
       });
   }, []);
 
@@ -19,62 +27,204 @@ export const Body: React.FC = () => {
     <Header/>
 
     <div className="rx-body-content-container">
-      <div className="rx-masonry-grid">
+      <div className="rx-body-content-container-flex">
+        <div className="rx-masonry-grid">
 
 
-        <div className="rx-masonry-grid-item">
-          <div className="rx-card">
-            Card
-            lorem ipsum lorem dolorem lorem ipsum lorem dolorem
-            lorem ipsum lorem dolorem
-            lorem ipsum lorem doloremlorem ipsum lorem dolorem
-            lorem ipsum lorem dolorem
-            lorem ipsum lorem dolorem
-            lorem ipsum lorem dolorem lorem ipsum lorem dolorem
-            lorem ipsum lorem dolorem
-            lorem ipsum lorem doloremlorem ipsum lorem dolorem
-            lorem ipsum lorem dolorem
-            lorem ipsum lorem dolorem
-            lorem ipsum lorem dolorem lorem ipsum lorem dolorem
-            lorem ipsum lorem dolorem
-            lorem ipsum lorem doloremlorem ipsum lorem dolorem
-            lorem ipsum lorem dolorem
-            lorem ipsum lorem dolorem
+          <div className="rx-masonry-grid-item">
+            <div className="rx-card">
+              Card
+              lorem ipsum lorem dolorem lorem ipsum lorem dolorem
+              lorem ipsum lorem dolorem
+              lorem ipsum lorem doloremlorem ipsum lorem dolorem
+              lorem ipsum lorem dolorem
+              lorem ipsum lorem dolorem
+              lorem ipsum lorem dolorem lorem ipsum lorem dolorem
+              lorem ipsum lorem dolorem
+              lorem ipsum lorem doloremlorem ipsum lorem dolorem
+              lorem ipsum lorem dolorem
+              lorem ipsum lorem dolorem
+              lorem ipsum lorem dolorem lorem ipsum lorem dolorem
+              lorem ipsum lorem dolorem
+              lorem ipsum lorem doloremlorem ipsum lorem dolorem
+              lorem ipsum lorem dolorem
+              lorem ipsum lorem dolorem
+            </div>
           </div>
-        </div>
-        <div className="rx-masonry-grid-item">
-          <div className="rx-card">Card lorem ipsum
-            adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
+          <div className="rx-masonry-grid-item">
+            <div className="rx-card">Card lorem ipsum
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+            </div>
           </div>
-        </div>
-        <div className="rx-masonry-grid-item">
-          <div className="rx-card">adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
+          <div className="rx-masonry-grid-item">
+            <div className="rx-card">adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+            </div>
           </div>
-        </div>
-        <div className="rx-masonry-grid-item">
-          <div className="rx-card">adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
+          <div className="rx-masonry-grid-item">
+            <div className="rx-card">adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+            </div>
           </div>
-        </div>
-        <div className="rx-masonry-grid-item">
-          <div className="rx-card">
-            adsdasdad askdas daksd ajsd ajsdk asdj
-            adsdasdad askdas daksd ajsd ajsdk asdj
+          <div className="rx-masonry-grid-item">
+            <div className="rx-card">
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+            </div>
+          </div>
+          <div className="rx-masonry-grid-item">
+            <div className="rx-card">
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+            </div>
+          </div>
+          <div className="rx-masonry-grid-item">
+            <div className="rx-card">
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+            </div>
+          </div>
+          <div className="rx-masonry-grid-item">
+            <div className="rx-card">
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+            </div>
+          </div>
+          <div className="rx-masonry-grid-item">
+            <div className="rx-card">
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+            </div>
+          </div>
+          <div className="rx-masonry-grid-item">
+            <div className="rx-card">adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+            </div>
+          </div>
+          <div className="rx-masonry-grid-item">
+            <div className="rx-card">adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+              adsdasdad askdas daksd ajsd ajsdk asdj
+            </div>
           </div>
         </div>
       </div>
     </div>
+
+    <h1>Masonry - columnWidth</h1>
+    <div className="grid">
+      <div className="grid-item"></div>
+      <div className="grid-item grid-item--width2 grid-item--height2"></div>
+      <div className="grid-item grid-item--height3"></div>
+      <div className="grid-item grid-item--height2"></div>
+      <div className="grid-item grid-item--width3"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item grid-item--height2"></div>
+      <div className="grid-item grid-item--width2 grid-item--height3"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item grid-item--height2"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item grid-item--width2 grid-item--height2"></div>
+      <div className="grid-item grid-item--width2"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item grid-item--height2"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item grid-item--height3"></div>
+      <div className="grid-item grid-item--height2"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item grid-item--height2"></div>
+    </div>
+
+    <div>
+      <div className="masonry-with-columns">
+        <div>
+          1 aldsladsadlsa sadksalksd asd lans dsb
+          1 aldsladsadlsa sadksalksd asd lans dsb
+          1 aldsladsadlsa sadksalksd asd lans dsb
+        </div>
+        <div>
+          2 dakdjslads alkdsbadsajbsd a dslhads
+          2 dakdjslads alkdsbadsajbsd a dslhads
+          2 dakdjslads alkdsbadsajbsd a dslhads
+          2 dakdjslads alkdsbadsajbsd a dslhads
+          2 dakdjslads alkdsbadsajbsd a dslhads
+          2 dakdjslads alkdsbadsajbsd a dslhads
+          2 dakdjslads alkdsbadsajbsd a dslhads
+        </div>
+        <div>
+          3 2 dakdjslads alkdsbadsajbsd a dslhads
+          2 dakdjslads alkdsbadsajbsd a dslhads
+        </div>
+        <div>
+          4
+          1 aldsladsadlsa sadksalksd asd lans dsb
+          1 aldsladsadlsa sadksalksd asd lans dsb
+        </div>
+        <div>
+          5
+        </div>
+        <div>
+          6
+        </div>
+        <div>
+          7v2 dakdjslads alkdsbadsajbsd a dslhads
+          2 dakdjslads alkdsbadsajbsd a dslhads
+          2 dakdjslads alkdsbadsajbsd a dslhads
+          2 dakdjslads alkdsbadsajbsd a dslhads
+        </div>
+        <div>
+          8
+        </div>
+        <div>
+          9
+        </div>
+        <div>
+          10
+        </div>
+        <div>
+          11
+        </div>
+        <div>
+          12
+        </div>
+        <div>
+          13
+        </div>
+        <div>
+          14
+        </div>
+        <div>
+          15
+        </div>
+      </div>
+    </div>
+
   </div>
 };
