e85eaf7cba2d7153f424562ade548ac6b4a802dd
Eduard Capanu <eduard.capanu@talentium.it>
2021-05-12 23:26:23 +0200

    feat: adding dropdown

--- src/google-keep/body/Body.tsx
+++ src/google-keep/body/Body.tsx
@@ -3,13 +3,14 @@ import { Header } from "./components/Header";
 import { add_alert, archive, image, more_vert, palette, persona_add_alt, pin_full } from "../../core/google-icons";
 import { IBoardNote } from "../../core/interfaces/IBoardState";
 import { useBody } from "./useBody";
+import { useOnClickOutside } from "../../core/hooks/useOnClickOutside";
 
 export const Body: React.FC = () => {
- const {
-   notes,
-   noop,
-   onCardOpenHandler
- } = useBody();
+  const {
+    notes,
+    noop,
+    onCardOpenHandler
+  } = useBody();
 
   return <div className='rx-body-container'>
     <Header/>
@@ -20,15 +21,15 @@ export const Body: React.FC = () => {
       <div className="rx-masonry-grid">
         {
           notes.map((note: IBoardNote, i: number) => {
-            return <div className="rx-masonry-grid-item rx-card" key={i} onClick={() => onCardOpenHandler(note)}>
-              <div className="rx-grid-content-header">
+            return <div className="rx-masonry-grid-item rx-card" key={i}>
+              <div className="rx-grid-content-header" onClick={() => onCardOpenHandler(note)}>
                 <div className="rx-grid-content-header-title">{note.title}</div>
                 <div className="rx-grid-content-header-icon">
                   <div className="icon-container xs-small">{pin_full}</div>
                 </div>
               </div>
 
-              <div className="rx-grid-content-body">
+              <div className="rx-grid-content-body" onClick={() => onCardOpenHandler(note)}>
                 <div
                   key={i}
                   id={i.toString()}
@@ -49,9 +50,7 @@ export const Body: React.FC = () => {
                 <div className="icon-container xs-small">{palette}</div>
                 <div className="icon-container xs-small">{image}</div>
                 <div className="icon-container xs-small">{archive}</div>
-                <div className="icon-container xs-small">{more_vert}
-                  <div className="dropdown-box">Delete</div>
-                </div>
+                <MoreActions />
               </div>
             </div>
           })
@@ -110,3 +109,35 @@ export const Body: React.FC = () => {
 
   </div>
 };
+
+
+export const MoreActions = () => {
+  const [isVisibleDropdown, setIsVisibleDropdown] = React.useState(false);
+  const boxRef = React.useRef<any | null>(null);
+  useOnClickOutside(boxRef, () => setIsVisibleDropdown(false));
+  return  <div className="icon-container xs-small" onClick={(e) => {setIsVisibleDropdown(true)}}>{more_vert}
+    {
+      isVisibleDropdown &&
+      <div ref={boxRef} className="dropdown-box">
+        <div className="dropdown-box-item">
+          Elimina note
+        </div>
+        <div className="dropdown-box-item">
+          Aggiungi etichetta
+        </div>
+        <div className="dropdown-box-item">
+          Aggiungi disegno
+        </div>
+        <div className="dropdown-box-item">
+          Crea una copia
+        </div>
+        <div className="dropdown-box-item">
+          Mostra caselle di controllo
+        </div>
+        <div className="dropdown-box-item">
+          Copia in documenti Google
+        </div>
+      </div>
+    }
+  </div>
+}
